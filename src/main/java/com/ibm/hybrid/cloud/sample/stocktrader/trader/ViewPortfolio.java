c54df0c3e374b0372b42c95789d0ccae68b49c8b
slombardo2 <60935040+slombardo2@users.noreply.github.com>
2020-10-06 12:03:50 -0400

    Update ViewPortfolio.java

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/ViewPortfolio.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/ViewPortfolio.java
@@ -181,7 +181,7 @@ public class ViewPortfolio extends HttpServlet {
 		writer.append("      <input type=\"submit\" name=\"submit\" value=\"Submit Feedback\" style=\"font-family: sans-serif; font-size: 16px;\"/>");
 		writer.append("    </form>");
 		writer.append("    <br/>");
-		writer.append("    <a href=\"https://github.com/IBMStockTrader/\">");
+		writer.append("    <a href=\"https://github.com/slombardo2/\">");
 		writer.append("      <img src=\"footer.jpg\"/>");
 		writer.append("    </a>");
 		writer.append("  </body>");
