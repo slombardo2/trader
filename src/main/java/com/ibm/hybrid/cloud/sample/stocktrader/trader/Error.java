e3ecf7658e4a0b11d6222f3bf66e5052afafc256
slombardo2 <60935040+slombardo2@users.noreply.github.com>
2020-10-06 12:01:41 -0400

    Update Error.java

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/Error.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/Error.java
@@ -54,7 +54,7 @@ public class Error extends HttpServlet {
 		writer.append("      <input type=\"submit\" name=\"submit\" value=\"Try again\" style=\"font-family: sans-serif; font-size: 16px;\">");
 		writer.append("    </form>");
 		writer.append("    <br/>");
-		writer.append("    <a href=\"https://github.com/IBMStockTrader/\">");
+		writer.append("    <a href=\"https://github.com/slombardo2/\">");
 		writer.append("      <img src=\"footer.jpg\"/>");
 		writer.append("    </a>");
 		writer.append("  </body>");
