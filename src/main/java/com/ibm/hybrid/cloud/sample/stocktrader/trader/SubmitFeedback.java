561e0eb98f6b16d4446d833bad0ad5cb4019e5df
slombardo2 <60935040+slombardo2@users.noreply.github.com>
2020-10-06 12:02:12 -0400

    Update SubmitFeedback.java

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/SubmitFeedback.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/SubmitFeedback.java
@@ -89,7 +89,7 @@ public class SubmitFeedback extends HttpServlet {
 		writer.append("      <input type=\"submit\" name=\"submit\" value=\"Cancel\" style=\"font-family: sans-serif; font-size: 16px;\" />");
 		writer.append("    </form>");
 		writer.append("    <br/>");
-		writer.append("    <a href=\"https://github.com/IBMStockTrader\">");
+		writer.append("    <a href=\"https://github.com/slombardo2\">");
 		writer.append("      <img src=\"footer.jpg\"/>");
 		writer.append("    </a>");
 		writer.append("  </body>");
